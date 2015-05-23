2c8c2b551bd143b1df5ed10aef317fdc4715dca5
Nick S <nsipplswezey@gmail.com>
2015-05-23 00:43:03 -0700

    Sets up testing dependencies, and first three tests!

--- string-calc-kata/src/string_calc_kata/core.clj
+++ string-calc-kata/src/string_calc_kata/core.clj
@@ -1,7 +1,11 @@
 (ns string-calc-kata.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+;;function params are defined as vector following the name of the function
+;;return values are whatever follows?
+(defn sum-a-string [string]
+  (if (empty? string)
+    0
+    (read-string string))
+  )
--- string-calc-kata/test/string_calc_kata/core_test.clj
+++ string-calc-kata/test/string_calc_kata/core_test.clj
@@ -1,7 +1,22 @@
 (ns string-calc-kata.core-test
-  (:require [clojure.test :refer :all]
+  (:require ;;[clojure.test :refer :all]
+   ;;instead of using clojure test, we use expectations
+            [expectations :refer :all]
             [string-calc-kata.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;this is the syntax for clojure test
+;;(deftest a-test
+;;  (testing "FIXME, I fail."
+;;    (is (= 0 1))))
+
+
+;;and this is the syntax for expecations
+(expect true (= 1 1))
+
+;;the simplest case of our string calculator
+;;an empty string should return 0
+(expect 0 (sum-a-string ""))
+
+;;A single digit should return itself
+(expect 1 (sum-a-string "1"))
+
